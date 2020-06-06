985324a76836206f5003731497a24d76df3ab446
Aadarsh Verma <aadarshv9@gmail.com>
2020-06-06 17:03:28 +0530

    Setup partials and layout

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 8000;
+const expressLayouts = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
 
 // use express router
 app.use('/', require('./routes'));
@@ -15,6 +24,5 @@ app.listen(port, function(err){
         console.log(`Error in running the server: ${err}`);
         return;
     }
-
     console.log(`Server is running on port: ${port}`);
 })
