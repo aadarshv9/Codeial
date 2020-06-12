35d372b83f6624336c156d3540146768b6c339ca
Aadarsh Verma <aadarshv9@gmail.com>
2020-06-13 01:03:22 +0530

    Setup manual-authentication

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
-const express = require("express");
+const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 const port = 8000;
 const expressLayouts = require('express-ejs-layouts');
 const db = require('./config/mongoose');
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
 
 app.use(express.static('./assets'));
 
@@ -21,9 +26,9 @@ app.set('views', './views');
 
 
 app.listen(port, function(err){
-    if(err){
+    if (err){
         console.log(`Error in running the server: ${err}`);
-        return;
     }
+
     console.log(`Server is running on port: ${port}`);
-})
+});
